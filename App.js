42934370f91f3479260533dc0d7e9127bee5f2f2
yuvrajraj2011 <Mudit.Bhatnagar@Rentacenter.com>
2021-07-06 23:38:11 -0500

    yes

--- App.js
+++ App.js
@@ -1,37 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React,{ useState} from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
-<<<<<<< HEAD
 import List from './Components/LatestMovies';
 import {Provider} from 'react-redux'
 import store  from './Store'
 import Navbar from "./Components/Navbar";
 
-=======
-import List from './Components/List';
-import {Provider} from 'react-redux'
-import store  from './Store'
->>>>>>> 16e1c2d0ca996b2335e1f97fb3ae8be00e470895
 
 export default function App() {
   const [text, setText ]= useState("Hello World!!")
   return (
-<<<<<<< HEAD
       <View >     
           <Navbar></Navbar>               
-=======
-    
-      <View style={styles.container}>
-        <Provider store={store}>
-        <Text style={styles.mainHeading}>Shopping Cart</Text>
-        <List></List>
-
-        {/* <Text>{text}</Text>
-        <Button title="Change Text" onPress={()=>{setText("Changed Text")}}></Button>
-          */}
-        <StatusBar style="auto" />
-        </Provider>
->>>>>>> 16e1c2d0ca996b2335e1f97fb3ae8be00e470895
        </View>
   );
 }
